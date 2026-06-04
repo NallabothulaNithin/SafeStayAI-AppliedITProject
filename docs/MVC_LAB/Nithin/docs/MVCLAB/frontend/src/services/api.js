ceeba9d5824e52d8fc631e1db0db4289de757ea1
Nithin <nithin@Nithins-MacBook-Air.local>
2026-06-04 20:13:47 +0200

    added mvclab folder

---
+++ docs/MVC_LAB/Nithin/docs/MVCLAB/frontend/src/services/api.js
@@ -0,0 +1,28 @@
+const BASE = "http://localhost:8000";
+
+export async function fetchTasks(){
+    const res = await fetch(`${BASE}/api/tasks/`);
+    
+    if (!res.ok) throw new Error("Failed to load tasks");
+    return res.json();
+}
+
+
+export async function createTask(title) {
+  const res = await fetch(`${BASE}/api/tasks/`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ title }),
+  });
+
+  if (!res.ok) throw new Error("Create failed");
+  return res.json();
+}
+
+export async function deleteTask(id) {
+  const res = await fetch(`${BASE}/api/tasks/${id}`, {
+    method: "DELETE",
+  });
+
+  if (!res.ok) throw new Error("Delete failed");
+}
